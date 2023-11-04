26df07cfb5eacb17cda8a2e03373eb94872fa60b
nahuel1111 <ricardo-nahuel@hotmail.com>
2023-11-04 16:36:08 -0300

    se arreglaron algunos problemas y se mejoro un poco el diseño

--- js/inicioUsuario.js
+++ js/inicioUsuario.js
@@ -6,14 +6,13 @@ let categoriaVino = document.getElementById("categoria-vino")
 let categoriaLicor = document.getElementById("categoria-licor")
 let categoriaWhisky = document.getElementById("categoria-whisky")
 let categoriaTragos = document.getElementById("categoria-tragos")
-console.log(destacadoBotella[0])
 destacado.innerHTML = `<div class="col-md-6 col-sm-12 responsive-cel centrar-caja">
 <div class="card d-flex centrado color-cambio text-light text-center mt-3 " style="width: 18rem;">
   <img src="../../IMAGENES/pro-1.png" class="card-img-top w-50" alt="...">
   <div class="card-body">
     <h5 class="card-title">${destacadoBotella[0].name}</h5>
     <p class="card-text ">$${destacadoBotella[0].price}</p>
-    <a href="#" class="boton3">Ver Producto</a>
+    <a href="#" class="boton3" onclick="irbotella(${destacadoBotella[0].id})">Ver Producto</a>
   </div>
 </div>
 </div>
@@ -23,7 +22,7 @@ destacado.innerHTML = `<div class="col-md-6 col-sm-12 responsive-cel centrar-caj
   <div class="card-body">
     <h5 class="card-title">${destacadoTragos[0].name}</h5>
     <p class="card-text">Ingredientes: <br>${destacadoTragos[0].ingred.ingred1}<br>${destacadoTragos[0].ingred.ingred2}<br> ${destacadoTragos[0].ingred.ingred3}</p>
-    <a href="#" class="boton3">Ver Producto</a>
+    <a href="#" class="boton3"  onclick="irvaso(${destacadoTragos[0].id})">Ver Producto</a>
   </div>
 </div>
 </div>
@@ -36,7 +35,6 @@ const paginavino = (event) => {
 
 const paginatequila = (event) => {
     event.preventDefault()
-    alert("hola")
     location.href=`/html/USUARIO/productosUsuario.html?licor=Tequila`
 }
 
@@ -49,7 +47,12 @@ const paginatragos = (event) => {
     event.preventDefault()
     location.href=`/html/USUARIO/productosUsuario.html?Tipo=Tragos`
 }
-
+const irbotella=(id)=>{
+  location.href=`/html/USUARIO/productoUsuario.html?botella=${id}`
+}
+const irvaso=(id)=>{
+  location.href=`/html/USUARIO/productoUsuario.html?trago=${id}`
+}
 categoriaVino.addEventListener("click", paginavino)
 categoriaLicor.addEventListener("click", paginatequila)
 categoriaWhisky.addEventListener("click", paginawhisky)
