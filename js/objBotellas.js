37d07e513880e886137ef57a2dca12138145718f
Tomas Bach <tomybach.90@gmail.com>
2023-11-07 21:14:44 -0300

    se modifica precio botellas

--- js/objBotellas.js
+++ js/objBotellas.js
@@ -7,7 +7,7 @@ if(botellasLs.length === 0){
         name: 'Johnnie Walker Blue Label',
         licor: 'Whisky',
         img: '/IMAGENES/bluelabel.png',
-        price: '221.962',
+        price: '221962',
         descrp: 'Un elisir para el paladar',
     },
     {
@@ -15,7 +15,7 @@ if(botellasLs.length === 0){
         name: 'The Macallan 18 Double Cask Single Malt Whisky Estuche 700ml',
         licor: 'Whisky',
         img: '/IMAGENES/macallan18.png',
-        price: '505.000',
+        price: '505000',
         descrp: 'El Macallan Double Cask 18 años, un whisky de pura malta perfectamente equilibrado, madurado durante 18 años en barricas de roble sazonadas con jerez americano y europeo. El roble americano sazonado con jerez agrega una delicada vainilla a las sutiles especias del roble europeo, brindando un whisky con un sabor y carácter más dulce y cálido.',
     },
     {
@@ -23,7 +23,7 @@ if(botellasLs.length === 0){
         name: 'Vino Eco Blanco 1 Lt Termidor Vinos En Tetra Brick',
         licor: 'Vino',
         img: '/IMAGENES/termidorblanco.png',
-        price: '1.738',
+        price: '1738',
         descrp: 'Un vino de mesa producido por el Grupo Peñaflor, quienes tienen unas bodegas destacadas a nivel internacional. Desde hace tiempo, el vino tinto Termidor se hace presente en la mesa de las familias argentinas, pero también es uno de los vinos más consumidos en todo el mundo',
     },
     {
@@ -31,7 +31,7 @@ if(botellasLs.length === 0){
         name: 'Tequila Don Julio Blanco 750ml',
         licor: 'Tequila',
         img: '/IMAGENES/donjulioblanco.png',
-        price: '68.000',
+        price: '68000',
         descrp: 'Todo el tequila Don Julio proviene de plantas 100% puras de agave Blue Weber. Cada gota que vive dentro de cada botella de tequila Don Julio nace en Los Altos de Jalisco, donde el microclima y el suelo de arcilla roja rica en minerales permiten que florezca la planta de agave azul.',
     },
     {
@@ -39,7 +39,7 @@ if(botellasLs.length === 0){
         name: 'Tequila Jose Cuervo Especial gold dorado 750ml',
         licor: 'Tequila',
         img: '/IMAGENES/josecuervodorado.png',
-        price: '15.233',
+        price: '15233',
         descrp: 'Un aroma a madera, nueces y avellanas; posee un sabor suave y dulce, de color ámbar, elaborado con el mejor agave azul recogido a mano, cocido lentamente, prensado y destilado, no es un proceso rápido sin embargo le otorga esa característica única',
     },
     {
@@ -47,7 +47,7 @@ if(botellasLs.length === 0){
         name: 'Vino Cordero Con Piel De Lobo Mb 750ml De Mosquita Muerta',
         licor: 'Vino',
         img: '/IMAGENES/corderoconpieldelobo.png',
-        price: '1.789',
+        price: '1789',
         descrp: 'Un Malbec joven muy bien elaborado, diferente por su aroma y sabor a frutos maduros. Con una acidez justa deja en boca un picor agradable con un final reforzado por su paso por madera.',
     },
     {
@@ -55,7 +55,7 @@ if(botellasLs.length === 0){
         name: 'Vino Angelica Zapata Malbec Alta 750cc',
         licor: 'Vino',
         img: '/IMAGENES/angelicazapata.png',
-        price: '14.386',
+        price: '14386',
         descrp: 'Presentado en una elegante botella, este vino es perfecto para compartir en ocasiones especiales o simplemente para disfrutar de una buena cena en casa. Su origen argentino y la cuidada selección de uvas de la región de Mendoza garantizan un sabor auténtico y una calidad excepcional.',
     },
     {
@@ -63,7 +63,7 @@ if(botellasLs.length === 0){
         name: 'Whiskey Jameson Irlandés 700ml',
         licor: 'Whiskey',
         img: '/IMAGENES/jameson.png',
-        price: '9.889',
+        price: '9889',
         descrp: 'Color dorado cálido. Aroma Suave y fragante, con notas de madera tostada y jerez. Su sabor es excepcionalmente suave, dulce y complejo. Delicada fusión de sabores tales como vainilla, madera de roble, miel, complementados por especias, caramelo y nuez.',
     },
     {
@@ -71,7 +71,7 @@ if(botellasLs.length === 0){
         name: 'Vino Andeluna Malbec Rose Edicion Limitada 750ml',
         licor: 'Vino',
         img: '/IMAGENES/vinorosado.jpg',
-        price: '2.979',
+        price: '2979',
         descrp: 'Honramos con pasión el suelo, la altura, Los Andes y la Luna. La altura precisa, el aire de la cordillera, el sol, el trabajo de nuestra gente dan a estos vinos elegancia, balance, amabilidad. Este vino representa la magia de su entorno, la expresión de su lugar.',
     },
     {
@@ -79,7 +79,7 @@ if(botellasLs.length === 0){
         name: 'Tequila blanco silver Jose Cuervo Especial 750ml',
         licor: 'Tequila',
         img: '/IMAGENES/josecuervoblanco.png',
-        price: '15.750',
+        price: '15750',
         descrp: 'Jose Cuervo Silver se dio a conocer a través de la muy famosa y mexicana bebida “Margarita”. Jose Cuervo es un tequila premium color blanco, perfecto para mezclar y preparar diferentes bebidas. Conocido en el mundo como Cuervo Gold, Jose Cuervo Especial se obtiene al ser reposado en barricas dobles logrando obtener un extraordinario suave y dulce sabor, con aroma a madera, nueces y avellanas.',
     },
 ]
